b76503a122b1456124fd81331c3c4245696e586f
Gabriel San Martin <gssm.snf19@uea.edu.br>
2021-06-09 20:47:20 -0400

    Factorial erlang program

--- erlang/factorial.erl
+++ erlang/factorial.erl
@@ -1,1 +1,13 @@
-% init
+% factorial program
+
+-module(factorial).
+
+-export([factorial/1]).
+
+factorial(N) ->
+    if
+        N < 2 ->
+            1;
+        true ->
+            N * factorial(N - 1)
+    end.
