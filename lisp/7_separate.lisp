f396e488c9742eb3e6ad5b842a7c1ee1ba455e51
Helder Melik Schramm <55168812+HelderSchramm@users.noreply.github.com>
2021-06-16 18:50:55 -0400

    Update 7_separate.lisp

--- lisp/7_separate.lisp
+++ lisp/7_separate.lisp
@@ -2,10 +2,10 @@
   (setq temp '())
   (setq even '())
   (setq odd '())
-  (dolist (i arr)                          ; for a in list_a
-    (when (equal (mod i 2) 0)              ; if a % 2 == 0
+  (dolist (i arr)                          
+    (when (equal (mod i 2) 0)              
       (push i even))
-    (when (equal (mod i 2) 1)              ; if a % 2 == 1
+    (when (equal (mod i 2) 1)              
       (push i odd))
   )
   (setq temp (cons (nreverse odd) temp))
